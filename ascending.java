a7bb363334e883603d978adf3494c96140e40786
milan ghodasara <milanghodasara73@gmail.com>
2026-03-02 19:30:45 +0530

    etc maked

---
+++ ascending.java
@@ -0,0 +1,15 @@
+//11th
+import java.util.Scanner;
+class ascending
+{
+    public static void main(String[] args)
+    {
+        int[] a ={5,8,9,7,4,10,6,1,2,3};
+        java.util.Arrays.sort(a);
+        
+		for(int i=0;i<a.Length;i++)
+		{
+			System.out.println(a[i]+" ");
+		}
+    }
+}
